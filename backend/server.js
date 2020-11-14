e7d540783f66aea6585fbc0ebbefe4fed56e0184
Akash Saxena <54790554+Akashsaxena2308@users.noreply.github.com>
2020-11-14 08:08:22 +0530

    Nodemon,Concurrently and ES6 features in Backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+dotenv.config();
 
 const app = express();
+
+app.get('/',(req,res) => {
+  res.send("API is running!")
+})
 
 app.get('/api/products', (req,res) => {
   res.json(products)
@@ -12,4 +19,6 @@ app.get('/api/products/:id',(req,res) => {
   res.json(product)
 })
 
-app.listen(5000,console.log("Server is runnning on Port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,console.log(`Server is running in ${process.env.NODE_ENV} mode on Port ${PORT}`));
